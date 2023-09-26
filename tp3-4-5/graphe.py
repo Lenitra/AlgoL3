d58eb46afb9681362395f5f7a912fc37e3b577c3
Thomas <lenitramc@gmail.com>
2023-09-26 10:49:43 +0200

    TP 3 terminé

--- tp3-4-5/graphe.py
+++ tp3-4-5/graphe.py
@@ -5,60 +5,91 @@ class Graphe():
         # on initialise le graphe sans aucun noeud
         
         # TODO
-        if matrice_adjacence:
-            self.matrice_adjacence = matrice_adjacence
+        if matrice_adjacence is None:
+            self.matrice_adjacence = {}
+        else:
+            for i in range(len(matrice_adjacence)):
+                self.matrice_adjacence[i] = []
+                for j in range(len(matrice_adjacence[i])):
+                    if matrice_adjacence[i][j] == 1:
+                        self.matrice_adjacence[i].append(j)
             
     
     def __str__(self):
-        return self.matrice_adjacence
+        return (self.matrice_adjacence.__str__() + "\n")
         
 
     def ajouter_noeud(self,u):
     # doit ajouter u  au graphe si celui-ci n'existe pas
     # ne fait rien si il existe déjà
         # ajouter une ligne et une colonne à la matrice d'adjacence
-        self.matrice_adjacence.append([0]*len(self.matrice_adjacence))
+        if self.matrice_adjacence.get(u) is None:
+            self.matrice_adjacence[u] = []
 
     def noeuds(self):
     # renvoie la liste des noeuds
         toret = []
-        for i in range(len(self.matrice_adjacence)):
-            toret.append(i)
+        for key in self.matrice_adjacence:
+            toret.append(key)
         return toret
 
     def ajouter_arete(self,u,v):
     # ajoute l'arête de u à v
     # on suppose que u et v existent
-        pass
+        self.matrice_adjacence[u].append(v)
+
 
     def enlever_arete(self,u,v):
     # enlève l'arête de u à v
     # ne fait rien si elle n'existe pas
-        pass
+        if v in self.matrice_adjacence[u]:
+            self.matrice_adjacence[u].remove(v)
 
     def enlever_noeud(self,u):
     # enlève le noeud u, pour garder la consistence,
     # enlève toutes les arêtes de et vers u
-        pass
+        for k, v in self.matrice_adjacence.items():
+            if u in v:
+                v.remove(u)
+        self.matrice_adjacence.pop(u)
+
+
 
     def voisins(self,u):
     # renvoie les voisins de u (ou un itérateur sur ceux-ci)
-        pass
+        return self.matrice_adjacence[u]
 
     def nb_sommets(self):
     # renvoie le nombre de sommets
-        pass
+        return len(self.matrice_adjacence)  
 
     def nb_aretes(self):
     # renvoie le nombre d'arêtes
-        pass
+        nb = 0
+        for key in self.matrice_adjacence:
+            nb += len(self.matrice_adjacence[key])
+        return nb
 
     def export_matrice(self):
     # renvoie une matrice d'adjacence pour
     # le graphe et un dictionnaire qui associe les
     # noms/identifiants aux indices correspondants 
     # de la matrice
-        pass
+        matrice = []
+        dico = {}
+        i = 0
+        for key in self.matrice_adjacence:
+            dico[key] = i
+            i += 1
+        for key in self.matrice_adjacence:
+            matrice.append([])
+            for j in range(i):
+                if j in self.matrice_adjacence[key]:
+                    matrice[dico[key]].append(1)
+                else:
+                    matrice[dico[key]].append(0)
+        return matrice, dico
+    
 
 
 
@@ -68,23 +99,19 @@ if __name__=="__main__":
 # mais ne le seront pas lors d'un import 
     
 
-    G1 = {
-            0 : {
-                1 : True,
-                2 : True,
-                3 : True,
-                },
-            1 : {
-                3 : True,
-                },
-            2 : {},
-            3 : {
-                0 : True,
-                1 : True,
-                },
-        }
+
     
-    G = Graphe(G1)
+    G = Graphe()
+    G.ajouter_noeud(0)
+    G.ajouter_noeud(1)
+    G.ajouter_noeud(2)
+    G.ajouter_arete(0,1)
+    G.ajouter_arete(0,2)
+    G.ajouter_arete(2,0)
+    
+    print(G)
 
-    print(G.noeuds())
-
+    print("noeuds" + str(G.noeuds()))
+    print("Nb sommets : " + str(G.nb_sommets()))
+    print("Nb aretes : " + str(G.nb_aretes()))
+    print("Voisin de 0 : " + str(G.voisins(0)))
